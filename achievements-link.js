b920bcc40454038a293ec6dd5b2a7b465d958527
Malcolm Smy <malcolm.smy@sky.uk>
2016-10-18 13:32:01 +0100

    moving to dom ready function

--- achievements-link.js
+++ achievements-link.js
@@ -7,18 +7,20 @@ Just draw a border round the document.body.
 
     init = function() {
 
+        var node = "<li> " +
+            "<a class='topnav-statenav-link' href='http://localhost:8080/'>" +
+            "<i class='icon-help topnav-statenav-linkicon'></i>" +
+            "<span class='is-tablet'>Achievements <i class='icon-chevron topnav-chevron'></i></span>" +
+            "</a>"+
+            "</li>";
+
+        $(".topnav .l-list-inline").prepend(node);
+
         //alert('dome ready');
     };
 
 
-    var node = "<li> " +
-        "<a class='topnav-statenav-link prevent-default' href='#'>" +
-        "<i class='icon-help topnav-statenav-linkicon'></i>" +
-            "<span class='is-tablet'>Achievements <i class='icon-chevron topnav-chevron'></i></span>" +
-        "</a>"+
-    "</li>";
 
-    $(".topnav .l-list-inline").prepend(node);
     //document.getElementById("list-inline").appendChild(node);
 
     $(init);

9942519d7fcd6f55b6d035a9ae1cc7f3a926744e
Malcolm Smy <malcolm.smy@sky.uk>
2016-10-18 13:52:49 +0100

    make tweet button work

--- dashboard/main.js
+++ dashboard/main.js
@@ -3,6 +3,9 @@ $( document ).ready(function() {
     console.log( "ready!" );
 
     move(80); // Could get from API
+
+    tweet()
+
 });
 
 function move(percentage) {
@@ -20,4 +23,13 @@ function move(percentage) {
             elem.style.width = width + '%';
         }
     }
+}
+
+function tweet() {
+    $('.tweet').click(function(e) {
+        e.preventDefault();
+
+        window.location.href = 'https://twitter.com/intent/tweet?text=Achievement%20Won!%20Come%20and%20challenge%20me!'
+
+    })
 }

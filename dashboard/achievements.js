0b44ab9873abfc07162666708efff83045ebe466
Malcolm Smy <malcolm.smy@sky.uk>
2016-10-18 11:50:48 +0100

    add achievements

---
+++ dashboard/achievements.js
@@ -0,0 +1,41 @@
+(function() {
+
+    var
+
+    container,
+
+    init = function() {
+
+        container = $('#you-may-like__achievements');
+
+        var req = {
+            url: "achievements.json",
+            success: function(d) {
+                console.log(d);
+                parse(d);
+            }
+        };
+
+        $.ajax(req);
+
+    },
+
+    parse = function(data) {
+
+        var list = $('<ul />');
+
+        data.achievements.forEach(function(i) {
+
+            var listItem = $('<li />');
+
+            listItem.html('<div>'+i.name+'</div><div>'+i.description+'</div>');
+            list.append(listItem)
+
+        });
+
+        container.append(list);
+    };
+
+    $(init);
+
+})();

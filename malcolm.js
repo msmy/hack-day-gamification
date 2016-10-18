3e1b8855f0a26248b5699ec2e807371577588dba
Malcolm Smy <malcolm.smy@sky.uk>
2016-10-18 11:12:14 +0100

    add notification on screen

--- malcolm.js
+++ malcolm.js
@@ -5,9 +5,55 @@ Just draw a border round the document.body.
 
     var
 
+    documentBody,
+
     init = function() {
 
-        alert('dome ready');
+        // documentBody = $('.hero-image-overlay');
+        documentBody = $('body');
+
+
+        documentBody.css({
+           position: 'relative', border: '3px solid red'
+        });
+
+        if ( window.location.pathname.includes('watch-entertainment/alan-partridges') ) {
+
+            console.log('load');
+            $('.icon-big-play').click(addOverlay);
+
+        }
+    },
+
+    addOverlay = function() {
+
+            var div = $('<div />');
+
+            div.append('<div style="padding: 0 20px; opacity: 1; background: black; top: 0; position:absolute; width: 300px; border: 1px solid #fff; height: 100px;"><h4 style="font-weight: bold;">Congratulations!</h4><i class="fa fa-trophy" aria-hidden="true"></i><span>You have started watching alan partridge</span></div>');
+
+
+            setTimeout(function() {
+              div.css({
+                  opacity: 1,
+                  'z-index': 1000000,
+                  position: 'absolute',
+                  top: 0,
+                  color: 'white'
+              });
+
+                documentBody.append(div);
+
+                setTimeout(function() {
+                    div.css({
+                        transition: 'opacity 5s ease-in-out 0s',
+                        opacity: 0
+                    })
+                }, 6000);
+
+
+                console.log('add overlay' ,div );
+            }, 5000);
+
     };
 
     $(init);

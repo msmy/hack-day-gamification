a6589cce037e0143234c674c0aaed218ac96b2e2
jamiemaher <jamiemaher_321@hotmail.com>
2016-10-18 11:51:19 +0100

    styled progress bar

--- dashboard/main.js
+++ dashboard/main.js
@@ -1,9 +1,19 @@
-function move() {
+// A $( document ).ready() block.
+$( document ).ready(function() {
+    console.log( "ready!" );
+
+    move(40);
+});
+
+function move(percentage) {
     var elem = document.getElementById("myBar");
     var width = 1;
     var id = setInterval(frame, 10);
     function frame() {
-        if (width >= 100) {
+        if(!percentage) {
+            percentage = 100;
+        }
+        if (width >= percentage) {
             clearInterval(id);
         } else {
             width++;
